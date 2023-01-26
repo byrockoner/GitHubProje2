3d18b911440d77ff27a35be63a84244bb2471d88
byrockoner <bayrakoner@gnail.com>
2023-01-26 19:39:09 +0300

    task 3 yapıldı

---
+++ src/Gun01/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba GitHub");
+
+        System.out.println("Task 2 Yapıldı");
+        System.out.println("Tekrar Merhaba ");
+
+    }
+}
